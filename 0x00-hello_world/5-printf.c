aec4e11520ecde58ad8779f708cea9db785c4c3a
Msdot001 <masudowolabi@gmail.com>
2023-09-07 17:23:20 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,12 +4,11 @@
  * main - Printf function
  *
  * Description - Use of printf function to print output
- *
  * Return 0
  */
 
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
