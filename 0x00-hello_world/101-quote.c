25e7e58d5bc8805b13c367dea8de4a6c68eec609
Msdot001 <masudowolabi@gmail.com>
2023-09-07 18:21:37 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 #include <unistd.h>
 /**
 * main - Entry point
-* REturn: Always 0 (Sucess)
+* Return: Always 0 (Sucess)
 */
 int main(void)
 {
-write(1,"and that piece of art is useful\" - Dora Korpar, 2015-19-19\n", 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-19-19\n", 59);
 exit(1);
 }
