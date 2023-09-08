0c6a93faa0961299c0e050624b65c26488caf463
Msdot001 <masudowolabi@gmail.com>
2023-09-07 18:19:24 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+* main - Entry point
+* REturn: Always 0 (Sucess)
+*/
+int main(void)
+{
+write(1,"and that piece of art is useful\" - Dora Korpar, 2015-19-19\n", 59);
+exit(1);
+}
