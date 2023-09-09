fc38d3c6e06c6378a11079e9e489561354618972
Msdot001 <masudowolabi@gmail.com>
2023-09-08 18:20:48 -0700

    putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
 /**
-* main - Entry point
-* 
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char ch = 'a';
 	int n = 0;
-        /* your code goes there */
+	/* your code goes there */
 	while (n < 26)
 	{
 		putchar(ch);
