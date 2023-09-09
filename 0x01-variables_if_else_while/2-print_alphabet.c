24ec03139bfe532bdd16eb54949979d6deb89ef8
Msdot001 <masudowolabi@gmail.com>
2023-09-08 18:14:36 -0700

    putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - Entry point
- *
- * Return: 0 Always (Sucess)
+* main - Entry point
+* 
+* Return: Always 0 (Sucess)
 */
-
 int main(void)
 {
 	char ch = 'a';
