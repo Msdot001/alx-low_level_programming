69ef2cd2621734fe170e3f1f041129992b52d9a3
Msdot001 <masudowolabi@gmail.com>
2023-09-10 06:43:22 -0700

    combine_two_digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,12 @@ int main(void)
 {
 	int i;
 	int j;
-
 	for (i = 0; i < 10; i++)
 	{
 		for (j = (i + 1); j < 10; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-
 			if (i != 8 || j != 9)
 			{
 				putchar(',');
@@ -26,8 +24,6 @@ int main(void)
 			}
 		}
 	}
-	
 	putchar('\n');
-
 	return (0);
 }	
