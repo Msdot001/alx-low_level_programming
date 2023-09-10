a757de402a30cc30ca7d311c2cfdfb093ac91def
Msdot001 <masudowolabi@gmail.com>
2023-09-10 07:17:18 -0700

    combine_three_digit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for( i = 0; i < 10; i++)
+	{
+		for(j = (i + 1); j < 10; j++)
+		{
+			for(k =(i + 2); k <10; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+			}
+			if ( i != 7; || j != 8; || k != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	
+	putchar('\n');
+	return (0);
+}
