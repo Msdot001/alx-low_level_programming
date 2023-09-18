0c94a9590ad6089aea71e0f1250ef949cfcecf22
Msdot001 <masudowolabi@gmail.com>
2023-09-18 10:25:58 -0700

    print_statement

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - to print a string
+ * @str: pointer of type char
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while(str[i] != '\0')
+	{
+		printf("%c", str[i]);
+		i++;
+	}
+	printf("\n");
+}
