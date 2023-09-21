1c190feaa159c130eda2bae6daa112f0a1fdd235
Msdot001 <masudowolabi@gmail.com>
2023-09-21 14:28:04 -0700

    capitalise_letter

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - capitalise a string
+ * @s: pointer to the string to be capitalised
+ * Return: pointer to the capitalised string
+ */
+
+char *string_toupper(char *a)
+{
+	int length;
+
+	length = 0;
+
+	while (a[length] != '\0')
+	{
+		if (a[length] >= 'a' && a[length] <= 'z')
+		{
+			a[length] = a[length] - 32;
+		}
+		length++;
+	}
+
+	return (a);
+}
