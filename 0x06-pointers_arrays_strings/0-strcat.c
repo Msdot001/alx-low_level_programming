91b8d79b76398572497298ecb987a4b869bbc26a
Msdot001 <masudowolabi@gmail.com>
2023-09-21 09:57:47 -0700

    concatenate_str

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strcat - to concatenates two strings.
+ *@dest: pointer of char data type
+ *@src: pointer of char data type
+ *Return: char 
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = src[j];
+	return (dest);
+}
