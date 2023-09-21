2e7db9a86344b4632465a4437b744a959daf4b45
Msdot001 <masudowolabi@gmail.com>
2023-09-21 11:47:37 -0700

    str_concatenate

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,20 +7,16 @@
  * @n: num of bytes to be append from src
  * Return: Always 0.
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int dest_len, i;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-	}
-	for (j = 0; src[j] != '\0' && j < n; j++)
-	{
-		dest[i + j] = src[j];
-	}
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+		;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+/*should end with a end of string char*/
+	dest[dest_len + i] = '\0';
 
-	dest[i + j] = '\0';
-
-	return(dest);
+	return (dest);
 }
