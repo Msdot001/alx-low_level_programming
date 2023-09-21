fac07c31aa8839de5302c7e10dc60b7c634f764f
Msdot001 <masudowolabi@gmail.com>
2023-09-21 10:32:38 -0700

    concatenate_

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - to concatenates two strings
+ * @dest: pointer intended to append the text
+ * @src: array with the source str
+ * Return: concatenate str
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+
+	}
+
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+
+	return(dest);
+}
