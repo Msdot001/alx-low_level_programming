2ed27948901b8b16a11e9503c136ccb1539779ca
Msdot001 <masudowolabi@gmail.com>
2023-09-21 12:16:01 -0700

    copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,9 +2,9 @@
 
 /**
  * _strncpy - to copies the string
- * @dest: 
- * @src
- * @n:
+ * @dest: pointer where the text is append
+ * @src: array with the string
+ * @n: num of bytes to append
  * Return: Always 0
  */
 
