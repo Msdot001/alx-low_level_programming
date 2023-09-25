20832bf1edac38363ead1db7f1cffa74437294d1
Msdot001 <masudowolabi@gmail.com>
2023-09-25 09:35:48 -0700

    locate_char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * strchr - locates a character in a string
+ * @s: pointer to string
+ * @c: character to locate
+ *
+ * Return: pointer to first occurence
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	if (c == '\0')
+	{
+		return (s);
+	}
+
+	return (NULL);
+}
