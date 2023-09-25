c0de9187d2d7a28eff14813ddb34244d55438a45
Msdot001 <masudowolabi@gmail.com>
2023-09-25 14:03:26 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strstr -  locates a substring
+ * @haystack: pointer to string
+ * @needle: pointer to string
+ * Return: pointer or Null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+    char *h, *n;
+
+    if (*needle == '\0')
+    {
+        return haystack;
+    }
+
+    while (*haystack != '\0')
+    {
+        h = haystack;
+        n = needle;
+
+        while (*n != '\0' && *h == *n)
+	{
+            h++;
+            n++;
+        }
+
+        if (*n == '\0')
+	{
+            return haystack;
+        }
+
+        haystack++;
+    }
+
+    return NULL;
+}
+
