c5f052c2299863f456ad063798253890a17d790b
Msdot001 <masudowolabi@gmail.com>
2023-09-25 14:25:53 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strstr -  locates a substring.
+ * *_strstr -  locates a substring.
  * @haystack: pointer to string.
  * @needle: pointer to string.
  * Return: pointer or Null.
