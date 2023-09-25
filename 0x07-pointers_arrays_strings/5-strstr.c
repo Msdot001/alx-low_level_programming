549f4e9b8e37d6d142ae12a87f6541ccce02131b
Msdot001 <masudowolabi@gmail.com>
2023-09-25 14:09:07 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 
     if (*needle == '\0')
     {
-        return haystack;
+        return (haystack);
     }
 
     while (*haystack != '\0')
@@ -29,11 +29,11 @@ char *_strstr(char *haystack, char *needle)
 
         if (*n == '\0')
 	{
-            return haystack;
+            return (haystack);
         }
 
         haystack++;
     }
 
-    return NULL;
+    return (NULL);
 }
