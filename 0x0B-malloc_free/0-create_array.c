ddf897f9a1be3b10d5ba6111677c69c3d8cb2e7e
Msdot001 <masudowolabi@gmail.com>
2023-10-04 15:48:13 -0700

    crete_array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * *create_array - creates an array of specific char
+ * @size: siz of the arrray
+ * @c: const array
+ * Return: pointer of an array , or NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+	return (NULL);
+	}
+
+	/* Allocate memory for the specific cher*/
+	
+	arr = malloc(size * sizeof(char));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
